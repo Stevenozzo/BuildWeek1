8b1f31a2d18965003b4cc7ce5806d197725e575a
Tamiele <tammaro_85@hotmail.it>
2024-09-02 20:02:21 +0200

    Update feedback.js

--- js/feedback.js
+++ js/feedback.js
@@ -1,9 +1,9 @@
 // 1) Rendere responsive le stelle
-const feedbackStars = document.querySelectorAll(".star");
-const myPath = document.getElementById("color");
+// const feedbackStars = document.querySelectorAll(".star");
+// const myPath = document.getElementById("color");
 
-feedbackStars.forEach(function (star) {
-  star.onclick = function () {
-    myPath.setAttribute("fill", "#00FFFF");
-  };
-});
+// feedbackStars.forEach(function (star) {
+//   star.onclick = function () {
+//     myPath.setAttribute("fill", "#00FFFF");
+//   };
+// });
