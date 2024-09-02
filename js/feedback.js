a438a4c3a8f6c1373458207674bf34872064852f
Stevenozzo <stevencarlino46@live.com>
2024-09-02 18:04:09 +0200

    Finezze

--- js/feedback.js
+++ js/feedback.js
@@ -1,2 +1,9 @@
 // 1) Rendere responsive le stelle
-const feedbackStars = document.getElementById("stars");
+const feedbackStars = document.querySelectorAll(".star");
+const myPath = document.getElementById("color");
+
+feedbackStars.forEach(function (star) {
+  star.onclick = function () {
+    myPath.setAttribute("fill", "#00FFFF");
+  };
+});
