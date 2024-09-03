8dd48696fe6ad25346fc5c79792e93d595776368
Andrea-LaVentura75 <laventuraandrea40@gmail.com>
2024-09-03 18:00:34 +0200

    add risposta feedback

--- js/feedback.js
+++ js/feedback.js
@@ -1,5 +1,23 @@
 const stars = document.querySelectorAll(".star svg");
 
+function hoverStars(event) {
+  const value = event.currentTarget.getAttribute("data-value");
+
+  stars.forEach((star) => {
+    if (parseInt(star.getAttribute("data-value")) <= parseInt(value)) {
+      star.querySelector("path").setAttribute("fill", "#00FFFF");
+    } else {
+      star.querySelector("path").setAttribute("fill", "#131d63");
+    }
+  });
+}
+
+stars.forEach((star) => {
+  star.addEventListener("mouseover", hoverStars);
+});
+
+let valutazione = [];
+
 function handleStarClick(event) {
   const value = event.currentTarget.getAttribute("data-value");
 
@@ -14,4 +32,18 @@ function handleStarClick(event) {
 
 stars.forEach((star) => {
   star.addEventListener("click", handleStarClick);
+});
+
+let button = document.querySelector(".styleButton");
+let footer = document.querySelector("footer");
+let h2 = document.querySelector("h2");
+button.addEventListener("click", function (event) {
+  const value = event.currentTarget.getAttribute("data-value");
+  stars.forEach((star) => {
+    if (parseInt(star.getAttribute("data-value")) <= 6) {
+    } else {
+      h2.innerText = "Grazie mille 😊😁";
+      footer.appendChild(h2);
+    }
+  });
 });
